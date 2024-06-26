f7670481fb6ec2f14d38631dd36e9b8585239768
haisombo <haisombo20@gmail.com>
2024-06-26 13:15:25 +0700

    Initial Commit

---
+++ QuickLookExtentionViewDoc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickLookExtentionViewDoc
+//
+//  Created by Hai Sombo on 6/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
